c14bccaa6352378388763fea4b520fdb9ef5c6a7
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2018-07-14 23:28:25 +0200

    Some notes and basic setup

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ Download from http://example.com/FIXME.
 
 ## Usage
 
-FIXME: explanation
+First, extract the images
+
+    $ pdfimages <pdf-file> img
+
+You now supposedly have a bunch of PPM-files. Remove unwanted images and convert to PNG:
+
+    $ convert \*.ppm img.png
+
+You now have a bunch of nicely numbered PNG-files.
 
     $ java -jar pnp-proc-0.1.0-standalone.jar [args]
 
--- src/pnp_proc/core.clj
+++ src/pnp_proc/core.clj
@@ -2,6 +2,19 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Take the following parameters:
+  FOR SOURCE
+  folder
+  offset x of second card
+  offset y of second card
+  card width
+  card height
+  columns
+  rows
+  FOR TARGET
+  number of cards in one page
+  horizontal and vertical margin
+  horzontal spacing
+  vertical spacing to fold line"
   [& args]
   (println "Hello, World!"))
