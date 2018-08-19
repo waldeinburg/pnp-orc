ba84ae5261e7e8efefe3867ec7b9cc64b220e792
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2018-08-19 21:15:44 +0200

    Changed the math of segmenting

---
+++ src/pnp_proc/calc.clj
@@ -0,0 +1,15 @@
+(ns pnp-proc.calc)
+
+(defn upper-left-from-second [[second-offset-x second-offset-y]
+                              [card-width card-height]]
+  [(- second-offset-x card-width)
+   (- second-offset-x card-height)])
+
+;; TODO: maybe we should get the PDRectangle of the original
+;; PDPage instead of using the image as point of departure.
+;; This will be easier to handle for e.g. Sprawlopolis which
+;; has several images in one page.
+(defn optimal-assemble-parameters [[img-width img-height]
+                                   [card-width card-height]]
+  "Calculate margins and number of cards per page based on data
+   from the original files")
--- src/pnp_proc/segmenting.clj
+++ src/pnp_proc/segmenting.clj
@@ -1,23 +1,21 @@
 (ns pnp-proc.segmenting
   (:require [mikera.image.core :as img]))
 
-(defn- get-cards-coordinates [[second-offset-x second-offset-y]
+(defn- get-cards-coordinates [[offset-x offset-y]
                               [card-width card-height]
                               [columns rows]]
-  (let [offset-x (- second-offset-x card-width)
-        offset-y (- second-offset-y card-height)]
-    (loop [c 0
-           r 0
-           coord-sets []]
-      (if (= r rows)
-        coord-sets
-        (let [new-c (mod (inc c) columns)
-              new-r (if (zero? new-c)
-                      (inc r)
-                      r)
-              coords [(+ offset-x (* card-width c))
-                      (+ offset-y (* card-height r))]]
-          (recur new-c new-r (conj coord-sets coords)))))))
+  (loop [c 0
+         r 0
+         coord-sets []]
+    (if (= r rows)
+      coord-sets
+      (let [new-c (mod (inc c) columns)
+            new-r (if (zero? new-c)
+                    (inc r)
+                    r)
+            coords [(+ offset-x (* card-width c))
+                    (+ offset-y (* card-height r))]]
+        (recur new-c new-r (conj coord-sets coords))))))
 
 (defn get-cards [image
                  offset-coords
