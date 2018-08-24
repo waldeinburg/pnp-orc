5a21e5f167844a74fe0390a02e18c9064291e807
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2018-08-24 22:28:46 +0200

    Most of recipe for actually making Palm Island

--- src/pnp_proc/games/palm_island.clj
+++ src/pnp_proc/games/palm_island.clj
@@ -1,6 +1,13 @@
-(ns pnp-proc.games.palm-island)
+(ns pnp-proc.games.palm-island
+  "Recipe for Palm Island, based on Palm-Island-Print-and-Play1.4.6.pdf"
+  (:require [pnp-proc.pdf     :as pdf]
+            [pnp-proc.worker  :as worker]
+            [pnp-proc.calc    :as calc]))
 
-(defn make []
-  ;; data for segmenting/get-cards
-  ;; [903 1130] [744 1040] [3 3]
-  )
+(defn make [pdf]
+  (let [card-dimensions [744 1040]
+        offset-coords (calc/upper-left-from-second [903 1130] card-dimensions)
+        images (pdf/get-images-from-pdf pdf)
+        img-sets (partition 2 images)
+        cards (worker/cards-from-image-sets img-sets
+                                            offset-coords card-dimensions [3 3])]))
