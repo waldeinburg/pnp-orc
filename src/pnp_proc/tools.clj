8bd21dfcbe3d51e5b3615b61d5aa611a341296e5
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2018-09-06 22:23:16 +0200

    Getting more info from PDF

--- src/pnp_proc/tools.clj
+++ src/pnp_proc/tools.clj
@@ -3,60 +3,99 @@
   (:require [mikera.image.core  :as img]
             [pnp-proc.file      :as file]
             [pnp-proc.pdf       :as pdf])
-  (:import org.apache.pdfbox.contentstream.operator.DrawObject
-           [org.apache.pdfbox.contentstream.operator.state
-            Concatenate Restore Save SetGraphicsStateParameters SetMatrix]
-           org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
-           org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject
-           org.apache.pdfbox.rendering.PDFRenderer
-           org.apache.pdfbox.contentstream.PDFStreamEngine
-           org.apache.pdfbox.pdmodel.PDDocument))
+  (:import
+    (org.apache.pdfbox.contentstream.operator DrawObject)
+    (org.apache.pdfbox.contentstream.operator.state Concatenate
+                                                    Restore
+                                                    Save
+                                                    SetGraphicsStateParameters
+                                                    SetMatrix)
+    (org.apache.pdfbox.pdmodel PDDocument PDResources)
+    (org.apache.pdfbox.pdmodel.graphics.image PDImageXObject)
+    (org.apache.pdfbox.pdmodel.graphics.form PDFormXObject)
+    (org.apache.pdfbox.contentstream PDFStreamEngine)
+    (org.apache.pdfbox.rendering PDFRenderer)
+    (org.apache.pdfbox.util Matrix)))
 
 (defn dump-images-from-pdf [pdf folder]
   "Dump all images from a PDF."
   (let [images (pdf/get-images-from-pdf pdf)]
     (file/save-images
-     images (str (clojure.java.io/file folder "img-%03d.png")))))
+      images (str (clojure.java.io/file folder "img-%03d.png")))))
 
-(defn- get-bitmap-dpi [page image-idx]
+;;; The method to get DPI can also be used to find the correct scale
+;;; and is therefore generalized.
+(defn- get-bitmap-attribute
+  "Find an attribute for an image on a page that is only accessible during
+   rendering.
+   The function f should have the signature:
+     [^PDImageXObject obj ^Matrix matrix].
+   Based on org.apache.pdfbox.examples.util.PrintImageLocations."
+  ([pdf page-idx image-idx f]
+   (pdf/with-open-doc [doc pdf]
+     (let [page (.getPage doc page-idx)]
+       (get-bitmap-attribute page image-idx f))))
+  ([page image-idx f]
+   (let [cur-img-idx (atom -1)
+         value (atom false)
+         attr-finder
+         (proxy [PDFStreamEngine] []
+           (processOperator [operator operands]
+             (if (= "Do" (.getName operator))
+               (let [obj (-> ^PDResources (.getResources this)
+                             (.getXObject (first operands)))]
+                 (cond
+                   (instance? PDImageXObject obj)
+                   (do
+                     (swap! cur-img-idx inc)
+                     (let [matrix (-> this .getGraphicsState
+                                      .getCurrentTransformationMatrix)
+                           new-v (f obj matrix)]
+                       (if (= @cur-img-idx image-idx)
+                         (reset! value new-v))))
+                   ;; Images may be embedded in forms.
+                   (instance? PDFormXObject obj)
+                   (.showForm this obj)))
+               (proxy-super processOperator operator operands))))]
+     ;; Yes, they are all necessary (cf. constructor in PrintImageLocations).
+     (.addOperator attr-finder (Concatenate.))
+     (.addOperator attr-finder (DrawObject.))
+     (.addOperator attr-finder (SetGraphicsStateParameters.))
+     (.addOperator attr-finder (Save.))
+     (.addOperator attr-finder (Restore.))
+     (.addOperator attr-finder (SetMatrix.))
+     ;; Get the value.
+     (.processPage attr-finder page)
+     @value)))
+
+(defn- dpi-fn [^PDImageXObject obj ^Matrix matrix]
+  "Helper for get-bitmap-dpi."
+  ;; The width in pixels divided by the width in inches
+  ;; gives the DPI to render by to match the bitmap.
+  ;; 72 DPI must be the default rendering for PDF's, it seems.
+  (/ (.getWidth obj)
+     (/ (.getScalingFactorX matrix) 72)))
+
+(defn- get-bitmap-dpi
   "Find the correct DPI for dumping a page as image so that bitmaps
    come out with their original dimensions.
-   Based on org.apache.pdfbox.examples.util.PrintImageLocations"
-  (let [cur-img-idx (atom -1)
-        dpi (atom false)
-        dpi-finder
-        (proxy [PDFStreamEngine] []
-          (processOperator [operator operands]
-            (if (= "Do" (.getName operator))
-              (let [obj (-> (.getResources this)
-                            (.getXObject (first operands)))]
-                (cond
-                  (instance? PDImageXObject obj)
-                  (do
-                    (swap! cur-img-idx inc)
-                    (let [matrix (-> this .getGraphicsState
-                                     .getCurrentTransformationMatrix)
-                          ;; The width in pixels divided by the width in inches
-                          ;; gives the DPI to render by to match the bitmap.
-                          ;; 72 DPI must be the default rendering for PDF's, it seems.
-                          new-dpi (/ (.getWidth ^PDImageXObject obj)
-                                     (/ (.getScalingFactorX matrix) 72))]
-                      (if (= @cur-img-idx image-idx)
-                        (reset! dpi new-dpi))))
-                  ;; Images may be embedded in forms.
-                  (instance? PDFormXObject obj)
-                  (.showForm this obj)))
-              (proxy-super processOperator operator operands))))]
-    ;; Yes, they are all necessary (cf. constructor in PrintImageLocations).
-    (.addOperator dpi-finder (Concatenate.))
-    (.addOperator dpi-finder (DrawObject.))
-    (.addOperator dpi-finder (SetGraphicsStateParameters.))
-    (.addOperator dpi-finder (Save.))
-    (.addOperator dpi-finder (Restore.))
-    (.addOperator dpi-finder (SetMatrix.))
-    ;; Get the value.
-    (.processPage dpi-finder page)
-    @dpi))
+   Based on org.apache.pdfbox.examples.util.PrintImageLocations."
+  ([page image-idx]
+   (get-bitmap-attribute page image-idx dpi-fn))
+  ;; For debugging.
+  ([pdf page-idx image-idx]
+   (get-bitmap-attribute pdf page-idx image-idx dpi-fn)))
+
+(defn get-bitmap-scaling-factor [pdf page-idx image-idx]
+  "Find the scale of an image on a page.
+   Used for producing the resulting PDF."
+  (get-bitmap-attribute pdf page-idx image-idx
+                        ;; The width in pixels divided by the width in inches
+                        ;; gives the DPI to render by to match the bitmap.
+                        ;; 72 DPI must be the default rendering for PDF's, it seems.
+                        (fn [_ ^Matrix matrix]
+                          (.getScalingFactorX matrix))))
+
 
 (defn render-page-to-image
   "Render a page to an image file.
@@ -66,7 +105,7 @@
    (pdf/with-open-doc [^PDDocument doc pdf]
      (let [renderer (PDFRenderer. doc)
            page (.getPage doc page-idx)
-           dpi (get-bitmap-dpi page image-idx)
+           dpi (get-bitmap-attribute page image-idx)
            image (.renderImageWithDPI renderer page-idx dpi)]
        (img/save image file)))))
 
